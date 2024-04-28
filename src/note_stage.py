75b1d847a21969bd9699336e6be6cf64bcc4c130
Daigo Tsuchiya <ogasawaradaigo5@icloud.com>
2024-04-28 21:54:34 +0900

    #0 add Copy function

--- src/note_stage.py
+++ src/note_stage.py
@@ -1,4 +1,5 @@
 import flet as ft
+import subprocess
 from .data_store import DataStore
 
 class NoteStage(ft.Container):
@@ -8,8 +9,6 @@ class NoteStage(ft.Container):
         self.page=page
         self.index=index
 
-
-        self.width=600
         self.bgcolor=ft.colors.TRANSPARENT
 
         self.stage = self.store.getStage(self.index)
@@ -18,19 +17,26 @@ class NoteStage(ft.Container):
             controls=[
                 ft.Text(
                     spans=[
-                        ft.TextSpan(f'stage{self.index+1} : '),
-                        ft.TextSpan(self.stage['subTitle'])
+                        ft.TextSpan(f'stage{self.index+1} : ',style=ft.TextStyle(size=15)),
+                        ft.TextSpan(self.stage['subTitle'],style=ft.TextStyle(size=17))
                     ],
                     color=ft.colors.BLACK,
                     style=ft.TextStyle(decoration=ft.TextDecoration.UNDERLINE)
                 ),
                 ft.Container(
-                  content=ft.Text(self.stage['cmd'],color=ft.colors.WHITE,bgcolor=ft.colors.TRANSPARENT,selectable=True),
-                  width=500,
-                  height=50,
-                  bgcolor=ft.colors.BLACK,
-                  alignment=ft.alignment.center_left,
+                    ft.Row(
+                        controls=[
+                            ft.IconButton(icon=ft.icons.CONTENT_COPY,on_click=self.clicked_copy()),
+                            ft.Text(self.stage['cmd'],color=ft.colors.WHITE,bgcolor=ft.colors.TRANSPARENT,selectable=True),
+                        ]  
+                    ),
+                    width=700,
+                    height=50,
+                    bgcolor=ft.colors.BLACK,
+                    alignment=ft.alignment.center_left,
                 ),
                 ft.Text(self.stage['description'],color=ft.colors.BLACK)
             ]
         )
+    def clicked_copy(self):
+        subprocess.run("clip", input=self.stage['cmd'], text=True)
